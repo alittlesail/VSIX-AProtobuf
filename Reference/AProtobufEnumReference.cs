c38beedd246d0baae75887a8f2d0ea401614751e
xingfanou <757609414@qq.com>
2020-08-08 11:14:36 +0800

    优化提示

--- Reference/AProtobufEnumReference.cs
+++ Reference/AProtobufEnumReference.cs
@@ -20,6 +20,16 @@ namespace ALittle
                 return parent.GetReference().GetFormateIndentation(offset, null);
             return 0;
         }
+
+        public override ABnfGuessError CheckError()
+        {
+            if (m_element.GetEnumName() == null)
+                return new ABnfGuessError(m_element, "没有定义枚举名");
+
+            if (m_element.GetEnumBody() == null)
+                return new ABnfGuessError(m_element, "没有定义协议内容");
+            return null;
+        }
     }
 }
 
--- Reference/AProtobufMessageReference.cs
+++ Reference/AProtobufMessageReference.cs
@@ -7,7 +7,7 @@ namespace ALittle
 
         public override int GetDesiredIndentation(int offset, ABnfElement select)
         {
-            ABnfElement parent = m_element.GetParent();
+			ABnfElement parent = m_element.GetParent();
             if (parent is AProtobufMessageBodyElement)
                 return parent.GetReference().GetDesiredIndentation(offset, null);
             return 0;
@@ -15,10 +15,20 @@ namespace ALittle
 
         public override int GetFormateIndentation(int offset, ABnfElement select)
         {
-            ABnfElement parent = m_element.GetParent();
+			ABnfElement parent = m_element.GetParent();
             if (parent is AProtobufMessageBodyElement)
                 return parent.GetReference().GetFormateIndentation(offset, null);
             return 0;
         }
-    }
+
+		public override ABnfGuessError CheckError()
+		{
+            if (m_element.GetMessageName() == null)
+                return new ABnfGuessError(m_element, "没有定义协议名");
+
+            if (m_element.GetMessageBody() == null)
+                return new ABnfGuessError(m_element, "没有定义协议内容");
+            return null;
+		}
+	}
 }
